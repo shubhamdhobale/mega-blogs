df4314d94d3af5f488e192d4fa8f49ee73554be6
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-04-24 23:23:49 +0530

    First Commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from '../conf/conf';
+import {Client ,Account , ID} from "appwrite";
+
+export class AuthService  {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+       .setEndpoint(conf.appwriteurl)
+       .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+
+  }
+
+  async createAccount({email, password, name}) {
+    try {
+        const userAccount = await this.account.create(ID.unique(), email, password, name);
+        if (userAccount) {
+            // call another method
+            return this.login({email, password});
+        } else {
+           return  userAccount;
+        }
+    } catch (error) {
+      console.log("Appwrite service :: createAccount Error" , error);
+        throw error;
+    }
+}
+
+async login({email ,password}){
+   try {
+    return await this.account.createEmailPasswordSession(email , password)
+   } catch (error) {
+    console.log("Appwrite service :: login Error", error);
+   }
+  }
+
+async getCurrentUser(){
+  try {
+    return await this.account.get();
+  } catch (error) {
+    console.log("Appwrite Error :: getCurrentUser error" , error);
+  }
+  return null;
+}
+
+async logout() {
+ try {
+  await  this.account.deleteSessions();
+ } catch (error) {
+  console.log("LogOut Failed", error);
+ }
+}
+
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ src/components/index.js
@@ -0,0 +1,40 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './container/Container'
+import Logo from "./Logo";
+import LogoutBtn from '../components/Header/LogoutBtn'
+import RTE from '../components/RTE'
+import Signup from './Signup';
+import Login from './Login';
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
+import AuthLayout from '../components/AuthLayout'
+import AllPost from '../pages/AllPost'
+import EditPost from '../pages/EditPost'
+import AddPost from '../pages/AddPost'
+import Post from '../pages/Post'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+
+
+export {
+  Header, 
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout,
+  AllPost,
+  EditPost,
+  AddPost,
+  Post,
+  Button,
+  Input,
+  Select
+}
